67fdf70f57c2c9ae7826ded89e28c6402941ae8b
Noah Finberg <noahfinberg@gmail.com>
2021-02-13 15:01:02 -0500

    require login to use secret key gen functionality.

--- keygen/views.py
+++ keygen/views.py
@@ -3,20 +3,25 @@ import time
 from django.shortcuts import render, redirect
 from django.contrib import messages
 
+from allauth.account.decorators import login_required
+
 from django_rq import job
 
 from .models import Secret
 
 # https://github.com/rq/django-rq#job-decorator
 @job
+@login_required
 def generate_bg():
     time.sleep(2) # Simulate expensive operation.
     Secret.objects.create()
 
+@login_required
 def index(request):
     context = {'secrets': Secret.objects.all()}
     return render(request, 'keygen/index.html', context)
 
+@login_required
 def generate(request):
     if request.GET.get('bg'):
         generate_bg.delay()
@@ -26,6 +31,7 @@ def generate(request):
         messages.success(request, 'Generated new key.')
     return redirect('home')
 
+@login_required
 def delete(request):
     Secret.objects.all().delete()
     messages.success(request, 'Deleted all keys.')
