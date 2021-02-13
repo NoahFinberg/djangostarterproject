c55e99a61b3792c44f6bf911a17126b6a436124c
Noah Finberg <noahfinberg@gmail.com>
2021-02-13 14:27:49 -0500

    modularize template and add basic form validation messages.

--- user/views.py
+++ user/views.py
@@ -5,5 +5,4 @@ from user.models import UserModel
 # Create your views here.
 @login_required
 def index(request):
-    html = "<html><body>you are logged in, welcome!</body></html>"
     return render(request, 'user/index.html')
